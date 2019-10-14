a5a18f0d9be5f011cafd301958ddb5787d9672ff
DaehanHong <45205334+DaehanHong@users.noreply.github.com>
2019-10-14 18:12:57 -0400

    Add files via upload

---
+++ makeGrayBatch/__SHELL3.java
@@ -0,0 +1,10 @@
+
+public class __SHELL3 extends bluej.runtime.Shell {
+public static void run() throws Throwable {
+final bluej.runtime.BJMap __bluej_runtime_scope = getScope("C:\\Users\\user\\Desktop\\coursera_Java\\course 2\\makeGrayBatch");
+final GrayScaleConverter grayScal1 = (GrayScaleConverter)__bluej_runtime_scope.get("grayScal1");
+
+
+grayScal1.selectAndConvert();
+
+}}
